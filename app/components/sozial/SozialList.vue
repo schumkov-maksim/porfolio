c39589de53fa075d347c805e0e73b7caad9a95de
Maksim <m.shumkov@einmahl-works.de>
2025-07-29 15:34:27 +0200

    store

--- app/components/sozial/SozialList.vue
+++ app/components/sozial/SozialList.vue
@@ -1,34 +1,18 @@
 <script setup lang="ts">
 import SozialItem from "./SozialItem.vue";
 
-const icons = [
-  {
-    name: "bxl:facebook",
-
-    url: "https://www.facebook.com/",
-  },
-  {
-    name: "bxl:instagram",
-
-    url: "https://www.facebook.com/",
-  },
-  {
-    name: "bxl:twitter",
-
-    url: "https://www.facebook.com/",
-  },
-  {
-    name: "bxl:tiktok",
-
-    url: "https://www.facebook.com/",
-  },
-];
+import { useStore } from "../../store/store";
+const store = useStore();
 </script>
 
 <template>
   <div class="">
     <div class="px-[40px] mt-8 flex gap-4">
-      <SozialItem v-for="item in icons" :name="item.name" :url="item.url" />
+      <SozialItem
+        v-for="item in store.list"
+        :name="item.name"
+        :url="item.url"
+      />
     </div>
     <div class="w-100 h-[1px] bg-stone-200/50 mt-5"></div>
   </div>
